95c5a641bfa3ad16bb9ce7e6bdbf88b458873779
matthew <matthew.butler@urbanmassage.com>
2016-03-23 17:13:52 +0000

    added: tests for overlaps & revoked locks

--- test.ts
+++ test.ts
@@ -6,8 +6,6 @@ let client:any = null;
 let RangeLock:any = null;
 let DB_URL:string = null;
 let rangeLock:any = null;
-let lockReleaseFromGet:Function = null;
-let lockReleaseFromSet:Function = null;
 
 describe('setup', () => {
   it('should complete', done => {
@@ -39,13 +37,15 @@ describe('setup', () => {
 });
 
 const testKey:string = 'lock::org_1::asset_1';
-const testFrom:number = new Date().getTime();
-const testTo:number = new Date().getTime() + 60*60*1000;
+let testFrom:number = new Date().getTime();
+let testTo:number = new Date().getTime() + 60*60*1000;
 const testData:string = JSON.stringify({test:1});
 const testTtl:number = 2*60*1000;
-let lockId:string = null;
 
 describe('interface', () => {
+  let lockId:string = null;
+  let lockReleaseFromGet:Function = null;
+  let lockReleaseFromSet:Function = null;
   it('should set', done => {
     rangeLock.set(testKey, testFrom, testTo, testData, testTtl, (err, success, lock) => {
       if (err) throw err;
@@ -66,19 +66,165 @@ describe('interface', () => {
       });
       assert(typeof lock.release == 'function', 'malformed lock release');
       lockReleaseFromGet = lock.release;
-      done()
+      done();
     });
   });
   it('should return releasable locks from set', done => {
     lockReleaseFromSet((err) => {
       if (err) throw err;
-      done()
+      done();
     });
   });
   it('should return releasable locks from get', done => {
     lockReleaseFromGet((err) => {
       if (err) throw err;
-      done()
+      done();
+    });
+  });
+});
+
+describe('overlaps', () => {
+  let lockReleases:Function[] = [];
+  let initialLockId:string = null
+
+  it('should set initial T+1hr>T+4hr', done => {
+    testFrom = new Date().getTime() + 60*60*1000;
+    testTo = new Date().getTime() + 4*60*60*1000;
+    rangeLock.set(testKey, testFrom, testTo, testData, testTtl, (err, success, lock) => {
+      if (err) throw err;
+      assert(success === true, 'success !== true');
+      assert(typeof lock.id == 'string', 'malformed lock id');
+      assert(typeof lock.release == 'function', 'malformed lock release');
+      assert(lock.expiry == parseInt(lock.expiry.toString(10)), 'malformed lock expiry');
+      initialLockId = lock.id;
+      lockReleases[0] = lock.release;
+      done();
+    });
+  });
+  it('should not set overlapping T+2hr>T+5hr', done => {
+    testFrom = new Date().getTime() + 2*60*60*1000;
+    testTo = new Date().getTime() + 5*60*60*1000;
+    rangeLock.set(testKey, testFrom, testTo, testData, testTtl, (err, success, lock) => {
+      if (err) throw err;
+      assert(success === false, 'success !== false');
+      assert(typeof lock.id == 'string', 'malformed lock id');
+      assert(lock.id == initialLockId, 'lock id not from blocking lock');
+      assert(typeof lock.release == 'function', 'malformed lock release');
+      assert(lock.expiry == parseInt(lock.expiry.toString(10)), 'malformed lock expiry');
+      lockReleases[1] = lock.release;
+      done();
+    });
+  });
+  it('should not set contained T+2hr>T+3hr', done => {
+    testFrom = new Date().getTime() + 2*60*60*1000;
+    testTo = new Date().getTime() + 3*60*60*1000;
+    rangeLock.set(testKey, testFrom, testTo, testData, testTtl, (err, success, lock) => {
+      if (err) throw err;
+      assert(success === false, 'success !== false');
+      assert(typeof lock.id == 'string', 'malformed lock id');
+      assert(lock.id == initialLockId, 'lock id not from blocking lock');
+      assert(typeof lock.release == 'function', 'malformed lock release');
+      assert(lock.expiry == parseInt(lock.expiry.toString(10)), 'malformed lock expiry');
+      lockReleases[2] = lock.release;
+      done();
+    });
+  });
+  it('should not set containing T+0hr>T+5hr', done => {
+    testFrom = new Date().getTime();
+    testTo = new Date().getTime() + 5*60*60*1000;
+    rangeLock.set(testKey, testFrom, testTo, testData, testTtl, (err, success, lock) => {
+      if (err) throw err;
+      assert(success === false, 'success !== false');
+      assert(typeof lock.id == 'string', 'malformed lock id');
+      assert(lock.id == initialLockId, 'lock id not from blocking lock');
+      assert(typeof lock.release == 'function', 'malformed lock release');
+      assert(lock.expiry == parseInt(lock.expiry.toString(10)), 'malformed lock expiry');
+      lockReleases[3] = lock.release;
+      done();
+    });
+  });
+  it('should set later T+5hr>T+6hr', done => {
+    testFrom = new Date().getTime() + 5*60*60*1000;
+    testTo = new Date().getTime() + 6*60*60*1000;
+    rangeLock.set(testKey, testFrom, testTo, testData, testTtl, (err, success, lock) => {
+      if (err) throw err;
+      assert(success === true, 'success !== true');
+      assert(typeof lock.id == 'string', 'malformed lock id');
+      assert(lock.id != initialLockId, 'lock id from blocking lock');
+      assert(typeof lock.release == 'function', 'malformed lock release');
+      assert(lock.expiry == parseInt(lock.expiry.toString(10)), 'malformed lock expiry');
+      initialLockId = lock.id;
+      lockReleases[0] = lock.release;
+      done();
+    });
+  });
+  it('should return releasable locks', done => {
+    function releaseNext(){
+      const release:Function = lockReleases.pop();
+      if (!release) {
+        return done();
+      } else {
+        return release(err => {
+          if (err) throw err;
+          releaseNext()
+        });
+      }
+    }
+    releaseNext();
+  });
+});
+
+describe('cancelling & relocking', () => {
+  let lockId:string = null;
+  let lockReleaseFromGet:Function = null;
+  let lockReleaseFromSet:Function = null;
+  it('should set', done => {
+    rangeLock.set(testKey, testFrom, testTo, testData, testTtl, (err, success, lock) => {
+      if (err) throw err;
+      assert(success === true, 'success !== true');
+      assert(typeof lock.id == 'string', 'malformed lock id');
+      assert(typeof lock.release == 'function', 'malformed lock release');
+      assert(lock.expiry == parseInt(lock.expiry.toString(10)), 'malformed lock expiry');
+      lockId = lock.id;
+      lockReleaseFromSet = lock.release;
+      done();
+    });
+  });
+  it('should return releasable locks from set', done => {
+    lockReleaseFromSet((err) => {
+      if (err) throw err;
+      done();
+    });
+  });
+  it('should not get', done => {
+    rangeLock.get(testKey, lockId, (err, lock) => {
+      if (err) throw err;
+      assert(lock === null, 'lock should be null');
+      if (lock){
+        return lock.release((err) => {
+          if (err) throw err;
+          done();
+        });
+      }
+      done();
+    });
+  });
+  it('should re-set', done => {
+    rangeLock.set(testKey, testFrom, testTo, testData, testTtl, (err, success, lock) => {
+      if (err) throw err;
+      assert(success === true, 'success !== true');
+      assert(typeof lock.id == 'string', 'malformed lock id');
+      assert(typeof lock.release == 'function', 'malformed lock release');
+      assert(lock.expiry == parseInt(lock.expiry.toString(10)), 'malformed lock expiry');
+      lockId = lock.id;
+      lockReleaseFromSet = lock.release;
+      done();
+    });
+  });
+  it('should return releasable locks from re-set', done => {
+    lockReleaseFromSet((err) => {
+      if (err) throw err;
+      done();
     });
   });
 });
